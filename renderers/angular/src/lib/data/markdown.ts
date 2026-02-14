4d0d44e166918ecdad985b9380672ff7378c2ffa
David Iglesias Teixeira <ditman@gmail.com>
2026-02-13 16:49:57 -0800

    Make the markdown renderer in angular noop.

--- renderers/angular/src/lib/data/markdown.ts
+++ renderers/angular/src/lib/data/markdown.ts
@@ -14,15 +14,74 @@
  limitations under the License.
  */
 
-import { /*inject,*/ Injectable } from '@angular/core';
-// import { DomSanitizer } from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
+import * as Types from "@a2ui/web_core/types/types";
 
-// TODO: Make this a noop/minimal Markdown renderer
+/**
+ * A minimal Markdown renderer that only supports a few tags.
+ *
+ * Configure @a2ui/markdown-it-angular, or build a custom Markdown renderer
+ * to actually parse and render Markdown in your app.
+ */
 @Injectable({ providedIn: 'root' })
 export class MarkdownRenderer {
-  // private sanitizer = inject(DomSanitizer);
 
-  render(value: string, tagClassMap?: Record<string, string[]>) {
-    return `<pre>${value}</pre>`;
+  static _warned = false;
+
+  constructor() {
+    /**
+     * Warn the user in case they need actual Markdown rendering.
+     */
+    if (!MarkdownRenderer._warned) {
+      console.warn("[MarkdownRenderer]",
+        "is a placeholder Markdown renderer. Most features are not supported.\n",
+        "Use `@a2ui/markdown-it-angular` for a fully-featured Markdown renderer.");
+      MarkdownRenderer._warned = true;
+    }
+  }
+
+  /**
+   * Attempts to render some Markdown.
+   *
+   * Resist the urge to "improve" this method. Instead, use a more proper
+   * Markdown renderer. This is just a placeholder.
+   */
+  render(markdown: string, _tagClassMap?: Types.MarkdownRendererTagClassMap) : string {
+    if (markdown.startsWith('# ')) {
+      let classes = this.getClasses("h1", _tagClassMap);
+      return `<h1 class="${classes}">${markdown.substring(2)}</h1>`;
+    }
+    if (markdown.startsWith('## ')) {
+      let classes = this.getClasses("h2", _tagClassMap);
+      return `<h2 class="${classes}">${markdown.substring(3)}</h2>`;
+    }
+    if (markdown.startsWith('### ')) {
+      let classes = this.getClasses("h3", _tagClassMap);
+      return `<h3 class="${classes}">${markdown.substring(4)}</h3>`;
+    }
+    if (markdown.startsWith('#### ')) {
+      let classes = this.getClasses("h4", _tagClassMap);
+      return `<h4 class="${classes}">${markdown.substring(5)}</h4>`;
+    }
+    if (markdown.startsWith('##### ')) {
+      let classes = this.getClasses("h5", _tagClassMap);
+      return `<h5 class="${classes}">${markdown.substring(6)}</h5>`;
+    }
+    if (markdown.startsWith('*') && markdown.endsWith('*')) {
+      let classes = this.getClasses("strong", _tagClassMap);
+      return `<strong class="${classes}">${markdown.substring(1, markdown.length - 1)}</strong>`;
+    }
+    let classes = this.getClasses("p", _tagClassMap);
+    return `<p class="${classes}">${markdown}</p>`;
+  }
+
+  /**
+   * Returns a space-separated lists of clases for `tag` from `_tagClassMap`.
+   * @param tag The name of the tag ("h1", "p", etc.)
+   * @param _tagClassMap The class map to use.
+   * @returns A space-separated list of class names.
+   */
+  getClasses(tag: string, _tagClassMap?: Types.MarkdownRendererTagClassMap) {
+    return _tagClassMap?.[tag]?.join(' ') ?? '';
   }
 }
